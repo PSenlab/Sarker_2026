17459c59755e26005a9a0e329b260999ed12cc9e
PSenlab <88839545+PSenlab@users.noreply.github.com>
2026-04-13 21:14:22 -0400

    Update validation_statin_GSE130991.py

--- human_cohort_validation/validation_statin_GSE130991.py
+++ human_cohort_validation/validation_statin_GSE130991.py
@@ -24,8 +24,6 @@
 # Output:
 #   verify_GSE130991_ASCL1_barplot.{png,pdf}
 #
-# Reference:
-#   Sarker et al. (2026) Cell Metabolism
 # ==============================================================================
 
 import os
