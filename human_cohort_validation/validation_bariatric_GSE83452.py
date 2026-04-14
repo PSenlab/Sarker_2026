6720a623d3aee52aae5d76c6ae4255f11df5847f
PSenlab <88839545+PSenlab@users.noreply.github.com>
2026-04-13 21:13:20 -0400

    Update validation_bariatric_GSE83452.py

--- human_cohort_validation/validation_bariatric_GSE83452.py
+++ human_cohort_validation/validation_bariatric_GSE83452.py
@@ -26,8 +26,6 @@
 # Output:
 #   verify_GSE83452_ASCL1_barplot.{png,pdf}
 #
-# Reference:
-#   Sarker et al. (2026) Cell Metabolism
 # ==============================================================================
 
 import os
