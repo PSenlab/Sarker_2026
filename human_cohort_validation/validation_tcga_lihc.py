f0090811151ff02ba295b7af724c221f24c80a8c
PSenlab <88839545+PSenlab@users.noreply.github.com>
2026-04-13 21:14:32 -0400

    Update validation_tcga_lihc.py

--- human_cohort_validation/validation_tcga_lihc.py
+++ human_cohort_validation/validation_tcga_lihc.py
@@ -24,8 +24,6 @@
 # Output:
 #   verify_TCGA_LIHC_ASCL1_barplot.{png,pdf}
 #
-# Reference:
-#   Sarker et al. (2026) Cell Metabolism
 # ==============================================================================
 
 import os
